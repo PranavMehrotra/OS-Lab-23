05a11d1eae978c984137be860fdb3e0d1f0a8771
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-01-21 12:19:44 +0530

    Many optimisations and changes

--- Assignments/A1/Q2/Q2.sh
+++ Assignments/A1/Q2/Q2.sh
@@ -1,6 +1,6 @@
 while read line;do
-    if([ ${#line} -ge 5 ]&&[ ${#line} -le 20 ]&&echo $line|grep -q '^[A-Za-z][A-Za-z]*[0-9][a-zA-Z0-9]*$')
-    then echo $line|grep -i -q -f fruits.txt &&echo "NO"|| echo "YES"
-    else echo "no"
+    if [[ ${#line} -ge 5 && ${#line} -le 20 && $line =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]]
+    then echo $line|grep -iqf fruits.txt &&echo "NO"|| echo "YES"
+    else echo "NO"
     fi
-done <$1
+done <$1 >"validation_results.txt"
--- Assignments/A1/Q7/Q7.sh
+++ Assignments/A1/Q7/Q7.sh
@@ -1,4 +1,4 @@
 mkdir -p "$2"
-for letter in {a..z};do
-    (echo "$(grep -ih "^$letter" $1/*.txt)" | sort) > $2/$letter.txt
+for alphabet in {a..z};do
+    (echo "$(grep -ih "^$alphabet" $1/*.txt)" | sort) > $2/$alphabet.txt
 done
