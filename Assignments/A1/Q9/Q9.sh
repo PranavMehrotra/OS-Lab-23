37dc050d7496b285f98026818d9601835a3b68d7
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-01-21 16:23:45 +0530

    minor changes

--- Assignments/A1/Q9/Q9.sh
+++ Assignments/A1/Q9/Q9.sh
@@ -1,3 +1,3 @@
-awk '{print $2}' $1 | sort | uniq -c | awk '{$1=$1; print $2"\t"$1}' | sort -k3 -k2nr
-awk '{print $1}' $1 | sort | uniq -d 
+awk '{print $2}' $1 | sort | uniq -c | awk '{print $2" "$1}' | sort -k3 -k2nr
+awk '{print $1}' $1 | sort | uniq -d
 awk '{print $1}' $1 | sort | uniq -u|wc -l
